c41c55917561a35e64b287119a79c04f264be8d0
SomuJoy <somu.joy12@gmail.com>
2024-07-04 19:32:31 +0530

    Added signup and login auth API's

---
+++ controllers/authController.js
@@ -0,0 +1,55 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.signup = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    let user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    user = new User({ username, password });
+    await user.save();
+
+    const payload = { user: { id: user.id } };
+
+    jwt.sign(payload, 'your_jwt_secret', { expiresIn: 3600 }, (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    let user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid credentials' });
+    }
+
+    const payload = { user: { id: user.id } };
+
+    jwt.sign(payload, 'your_jwt_secret', { expiresIn: 3600 }, (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
